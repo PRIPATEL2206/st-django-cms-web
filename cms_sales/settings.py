553eb4ee3b2bc54e66d27498f18093325ff648ce
prince <pr63547890@gmail.com>
2024-01-03 16:58:50 +0530

    static file problem

--- cms_sales/settings.py
+++ cms_sales/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STORAGES = {
 # …
-"static": {
+"staticfiles": {
 "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
 },
 }
