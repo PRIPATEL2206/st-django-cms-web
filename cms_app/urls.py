ca65df8db40e8d83b14a899c94f8519aa6cdba86
prince <pr63547890@gmail.com>
2024-01-03 17:16:02 +0530

    static file problem

--- cms_app/urls.py
+++ cms_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from cms_sales import settings
+from django.views.static import serve
 
 from . import views
 
@@ -37,3 +38,5 @@ urlpatterns = [
 ]
 if(settings.DEBUG):
     urlpatterns+=static(settings.STATIC_URL)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns+= re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
